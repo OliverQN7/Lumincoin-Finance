f2106d3bf0e2ab87dd5b6dad724d0035698ceb5b
Ilgiz Usmanov <oliverqni7@gmail.com>
2024-09-13 19:07:30 +0300

    buggy first version

---
+++ script/index.js
@@ -0,0 +1,22 @@
+import {Chart} from "chart.js";
+
+const ctx = document.getElementById("income-chart");
+
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ["Red", "Orange", "Yellow", "Green", "Blue"],
+        datasets: [{
+            label: 'Доходы',
+            data: [50, 70, 40, 30, 20],
+            backgroundColor: [
+                'rgb(218,53,69)',
+                'rgb(251,125,20)',
+                "rgb(253,192,7)",
+                "rgb(32,200,150)",
+                "rgb(13,109,251)"
+            ],
+            hoverOffset: 4
+        }]
+    }
+})
